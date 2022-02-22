f1233538cc187c4d0cce585d0d950486cf7e254c
lfilipova <lucijaf44@gmail.com>
2022-02-22 23:20:15 +0100

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name ="Serial Number")]
+        public string SerialNumber { get; set; }
+
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+        public ICollection<BorrowHistory> BorrowHistories { get; set; }
+
+
+    }
+}
